ccc0896f454c5115952e26d8121ad22e3426ad27
Pierre Mourlanne <pmourlanne@gmail.com>
2017-03-23 10:27:53 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import join, dirname
 from setuptools import setup, find_packages
 
 
-VERSION = (0, 2, 0)
+VERSION = (0, 3, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
