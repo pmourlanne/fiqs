ce2c8c74fe9bd4450150aa175a114e2b1473b553
Pierre Mourlanne <pmourlanne@gmail.com>
2020-01-13 13:44:12 +0100

    Fixed setup.py test configuration

--- fiqs/tests/run_tests.py
+++ fiqs/tests/run_tests.py
@@ -11,12 +11,12 @@ def run_all(argv=None):
         argv = [
             '--cov', 'fiqs', '--verbose',
             '--junitxml', 'junit.xml', '--cov-report', 'xml',
-            '-m "not docker and not performance"',
+            '-m', 'not docker and not performance',
         ]
     else:
         argv = argv[1:]
 
-    sys.exit(pytest.main(['fiqs'] + argv))
+    sys.exit(pytest.main(argv))
 
 
 if __name__ == '__main__':
