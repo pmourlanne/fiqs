5f3b5d10b8d4046e22de034dae9c1ae3149359dd
Pierre Mourlanne <pmourlanne@gmail.com>
2020-02-17 11:46:50 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 from setuptools.command.sdist import sdist
 
 
-VERSION = (0, 3, 35)
+VERSION = (0, 4, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
